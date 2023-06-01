f9ed60711293ad690c15bfd2c4d7fc3c182e914f
thais de rodez <thaisderodez@gmail.com>
2023-06-01 18:45:36 +0200

    feat(home): homepage layout

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,3 +1,15 @@
-export function Home () {
-  return <div>Index</div>
+import React from 'react'
+import type { ReactElement } from 'react'
+import { Header } from '../components/header'
+import { Link } from 'react-router-dom'
+import { EmployeeForm } from '../components/employeeForm'
+
+export function Home(): ReactElement {
+  return (
+    <>
+      <Header title='HRnet' />
+      <Link to="/employees" className='link'> View Current Employees</Link>
+      <EmployeeForm />
+    </>
+  )
 }
