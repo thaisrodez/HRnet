4c448e181ed1fe5d1bb5889ba6d34ab1b29260be
thais de rodez <thaisderodez@gmail.com>
2023-06-14 16:51:56 +0200

    feat(form): open modal on submit form

--- src/components/employeeForm.tsx
+++ src/components/employeeForm.tsx
@@ -2,6 +2,7 @@ import React, { useState, type ReactElement } from 'react'
 import { AddressForm } from './addressForm'
 import { Select } from './UI/select'
 import { departments } from '../data/selectData'
+import { Modal } from './UI/modal'
 
 export interface FormInputs {
   firstname: string
@@ -16,6 +17,7 @@ export interface FormInputs {
 }
 
 export function EmployeeForm(): ReactElement {
+  const [openModal, setOpenModal] = useState(false)
   const [formInputs, setFormsInputs] = useState({
     firstname: '',
     lastname: '',
@@ -38,45 +40,48 @@ export function EmployeeForm(): ReactElement {
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
     e.preventDefault()
+    setOpenModal(true)
     console.log(formInputs)
   }
 
   return (
-    <div className='mx-auto w-1/2'>
-      <h2 className='h2'>Create Employee</h2>
-      <form className='flex flex-col space-y-8' onSubmit={handleSubmit}>
-        <div className='space-y-4'>
-          <div>
-            <label htmlFor="first-name" className='label'>First Name</label>
-            <input type="text" id="first-name" name="firstname" value={formInputs.firstname} onChange={handleChange} className="input" />
+    <>
+      <div className='mx-auto w-1/2'>
+        <h2 className='h2'>Create Employee</h2>
+        <form className='flex flex-col space-y-8' onSubmit={handleSubmit}>
+          <div className='space-y-4'>
+            <div>
+              <label htmlFor="first-name" className='label'>First Name</label>
+              <input type="text" id="first-name" name="firstname" value={formInputs.firstname} onChange={handleChange} className="input" />
+            </div>
+            <div>
+
+              <label htmlFor="last-name" className='label'>Last Name</label>
+              <input type="text" id="last-name" name="lastname" value={formInputs.lastname} onChange={handleChange} className="input" />
+            </div>
+            <div>
+
+              <label htmlFor="date-of-birth" className='label'>Date of Birth</label>
+              <input id="date-of-birth" type="date" name='dateOfBirth' value={formInputs.dateOfBirth} onChange={handleChange} className="input" />
+            </div>
+            <div>
+
+              <label htmlFor="start-date" className='label'>Start Date</label>
+              <input id="start-date" type="date" name="startDate" value={formInputs.startDate} onChange={handleChange} className="input" />
+            </div>
+
+            <AddressForm handleChange={handleChange} handleSelectChange={handleSelectChange} formInputs={formInputs} />
+
+            <div>
+              <Select label="Deparment" name="department" id="department" options={departments} onChange={handleSelectChange} />
+            </div>
+
           </div>
-          <div>
 
-            <label htmlFor="last-name" className='label'>Last Name</label>
-            <input type="text" id="last-name" name="lastname" value={formInputs.lastname} onChange={handleChange} className="input" />
-          </div>
-          <div>
-
-            <label htmlFor="date-of-birth" className='label'>Date of Birth</label>
-            <input id="date-of-birth" type="date" name='dateOfBirth' value={formInputs.dateOfBirth} onChange={handleChange} className="input" />
-          </div>
-          <div>
-
-            <label htmlFor="start-date" className='label'>Start Date</label>
-            <input id="start-date" type="date" name="startDate" value={formInputs.startDate} onChange={handleChange} className="input" />
-          </div>
-
-          <AddressForm handleChange={handleChange} handleSelectChange={handleSelectChange} formInputs={formInputs} />
-
-          <div>
-            <Select label="Deparment" name="department" id="department" options={departments} onChange={handleSelectChange} />
-          </div>
-
-        </div>
-
-        <button className='btn'>Save</button>
-
-      </form>
-    </div>
+          <button className='btn'>Save</button>
+        </form>
+      </div>
+      <Modal open={openModal} setOpenModal={setOpenModal} content='Employee Created!' />
+    </>
   )
 }
