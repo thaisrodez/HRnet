92f2c30757146a7a6b8cc7202ca411311ebb33df
thais de rodez <thaisderodez@gmail.com>
2023-06-30 09:29:36 +0200

    fix(header): header layout

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 
 export function Header({ title, linkLabel, linkPath }: { title: string, linkLabel: string, linkPath: string }): ReactElement {
   return (
-    <div className='p-4 w-full text-center space-y-4'>
+    <div className='p-4 text-center space-y-4 mx-auto w-fit'>
       <h1 className='h1'>{title}</h1>
       <Link to={linkPath} className='link block'>{linkLabel}</Link>
     </div>
